6c78654f48b474ef8250dc82e28708583a09298c
pragathip07 <ppragthi10@gmail.com>
2024-08-22 21:50:21 +0530

    Add first link model

---
+++ app/controllers/links_controller.rb
@@ -0,0 +1,23 @@
+class LinksController < ApplicationController
+    def index
+        # @links = Link.order(created_at: :desc)
+        @links = Link.recent_first
+    end
+
+    def create
+        @link = Link.new(link_params)
+        if @link.save
+            redirect_to root_path
+        else
+            # @links = Link.recent_first
+            index
+            render :index, status: :unprocessable_entity
+        end
+    end
+
+    private
+
+    def link_params
+        params.require(:link).permit(:url)
+    end
+end
